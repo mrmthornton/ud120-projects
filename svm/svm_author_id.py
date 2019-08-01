642dad1863a4451f84ef6b3d704e0ac8f30b4071
mrmthornton <mrmthornton@gmail.com>
2019-08-01 16:47:27 -0400

    finished bayes and svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,15 +19,33 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
-import svm
 
-if __name__ == '__main__':
-    svm
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#clf = svm.SVC(kernel="linear")
+#clf = svm.SVC(kernel="rbf", C=10)
+#clf = svm.SVC(kernel="rbf", C=100)
+#clf = svm.SVC(kernel="rbf", C=1000)
+clf = svm.SVC(kernel="rbf", C=10000)
+
+##features_train = features_train[:len(features_train)/100]
+##abels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print "Training time ", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "Predicting time ", round(time() - t0, 3), "s"
+
+print sum(pred)
+
+print accuracy_score(labels_test, pred)
+
 
 #########################################################
 
