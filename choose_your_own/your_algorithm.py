deacf9d0883147e7c436ff262062ef2075384122
mrmthornton <mrmthornton@gmail.com>
2019-08-05 18:43:18 -0400

    finished KNN, working on adaboost

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -15,7 +15,6 @@ bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
-
 #### initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
@@ -24,18 +23,102 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+#plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+### KNN classifier
+'''from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+clf = KNeighborsClassifier(n_neighbors=8).fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, pred)
+'''
 
+### adaboost classifier ###
 
+# modified from example code by
+#          Author: Noel Dawe <noel.dawe@gmail.com>'
+#          License: BSD 3 clause
 
+import numpy as np
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_gaussian_quantiles
 
+#, , features_test, labels_test
+X = features_train
+y = labels_train
+
+# Create and fit an AdaBoosted decision tree
+bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
+                         algorithm="SAMME",
+                         n_estimators=200)
+
+bdt.fit(X, y)
+
+plot_colors = "br"
+plot_step = 0.02
+class_names = "AB"
+
+plt.figure(figsize=(10, 5))
+
+# Plot the decision boundaries
+plt.subplot(121)
+#x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+x_min = min([v for v,w in X]) -1
+x_max = max([v for v,w in X]) +1
+#y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+y_min = min([w for v,w in X]) -1
+y_max = max([w for v,w in X]) +1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                     np.arange(y_min, y_max, plot_step))
+
+Z = bdt.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+plt.axis("tight")
+
+# Plot the training points
+for i, n, c in zip(range(2), class_names, plot_colors):
+    idx = np.where(y == i)
+    test = X[idx][0]
+    plt.scatter(X[idx][ 0], X[idx][ 1],
+                c=c, cmap=plt.cm.Paired,
+                label="Class %s" % n)
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.legend(loc='upper right')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Decision Boundary')
+
+# Plot the two-class decision scores
+twoclass_output = bdt.decision_function(X)
+plot_range = (twoclass_output.min(), twoclass_output.max())
+plt.subplot(122)
+for i, n, c in zip(range(2), class_names, plot_colors):
+    plt.hist(twoclass_output[y == i],
+             bins=10,
+             range=plot_range,
+             facecolor=c,
+             label='Class %s' % n,
+             alpha=.5)
+x1, x2, y1, y2 = plt.axis()
+plt.axis((x1, x2, y1, y2 * 1.2))
+plt.legend(loc='upper right')
+plt.ylabel('Samples')
+plt.xlabel('Score')
+plt.title('Decision Scores')
+
+plt.tight_layout()
+plt.subplots_adjust(wspace=0.35)
+plt.show()
 
 
 try:
