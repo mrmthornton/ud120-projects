7a048a3534670492dbfb10f1c1b160a2bcd79e6e
mrmthornton <mrmthornton@gmail.com>
2019-10-15 22:43:45 -0400

    done with validation, moving to evaluation metrics

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -10,7 +10,8 @@ The dataset used in this example is a preprocessed excerpt of the
 
   .. _LFW: http://vis-www.cs.umass.edu/lfw/
 
-  original source: http://scikit-learn.org/stable/auto_examples/applications/face_recognition.html
+  original source:
+  http://scikit-learn.org/stable/auto_examples/applications/face_recognition.html
 
 """
 
@@ -61,7 +62,8 @@ print "n_classes: %d" % n_classes
 
 ###############################################################################
 # Split into a training and testing set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = \
+                    train_test_split(X, y, test_size=0.30, random_state=42)
 
 # ##############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
@@ -69,7 +71,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 n_components = 75 # tested 10, 15, 25, 50 75, 100, 150, 200, 250
 
 
-print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
+print "Extracting the top %d eigenfaces from %d faces" \
+                                            % (n_components, X_train.shape[0])
 t0 = time()
 pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
@@ -90,10 +93,10 @@ print "done in %0.3fs" % (time() - t0)
 
 print "Fitting the classifier to the training set"
 t0 = time()
-param_grid = {
-         'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-          'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
-          }
+param_grid = [{
+            'C': [1e2, 5e2, 1e3, 5e3, 1e4, 5e4, 1e5],
+            'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]
+          }]
 # for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
 clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
 clf = clf.fit(X_train_pca, y_train)
